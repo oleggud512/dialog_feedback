05e64abd9b6cd1bbe2a188f320520e090be50b57
oleggud512 <oleggud52@gmail.com>
2026-07-16 20:11:17 +0200

    init commit

---
+++ android/app/src/main/kotlin/com/example/dialog_feedback/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dialog_feedback
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/features.dart
@@ -0,0 +1,3 @@
+export 'history/history.dart';
+export 'settings/settings.dart';
+export 'training/training.dart';
