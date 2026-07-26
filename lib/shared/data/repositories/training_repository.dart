3aa62082304968bbb1b9dff8a5338c74bf14ec95
oleggud512 <oleggud52@gmail.com>
2026-07-26 17:43:43 +0200

    Updated formatting

--- lib/shared/data/repositories/training_repository.dart
+++ lib/shared/data/repositories/training_repository.dart
@@ -18,52 +18,43 @@ class TrainingRepositoryImpl with ActionExecutor implements TrainingRepository {
 
   @override
   Future<Result<Training>> createTraining(CreateTrainingParams params) async {
-    return execute(
-      () async {
-        final res = await db
-            .into(db.trainingTable)
-            .insert(
-              TrainingTableCompanion.insert(
-                initialTaskText: params.initialTaskText,
-              ),
-            );
+    return execute(() async {
+      final res = await db
+          .into(db.trainingTable)
+          .insert(
+            TrainingTableCompanion.insert(
+              initialTaskText: params.initialTaskText,
+            ),
+          );
 
-        return getTraining(res);
-      },
-      createDefault: (_) => Failure(DatabaseFailure()),
-    );
+      return getTraining(res);
+    }, createDefault: (_) => Failure(DatabaseFailure()));
   }
 
   Future<Result<Training>> getTraining(int id) async {
-    return execute(
-      () async {
-        final query = db.select(db.trainingTable)
-          ..where((tbl) => tbl.id.equals(id));
+    return execute(() async {
+      final query = db.select(db.trainingTable)
+        ..where((tbl) => tbl.id.equals(id));
 
-        final res = await query.getSingleOrNull();
+      final res = await query.getSingleOrNull();
 
-        if (res == null) {
-          return Failure(NotFoundFailure());
-        }
+      if (res == null) {
+        return Failure(NotFoundFailure());
+      }
 
-        return Success(_toDomain(res));
-      },
-      createDefault: (_) => Failure(DatabaseFailure()),
-    );
+      return Success(_toDomain(res));
+    }, createDefault: (_) => Failure(DatabaseFailure()));
   }
 
   @override
   Future<Result<List<Training>>> getTrainings() async {
-    return execute(
-      () async {
-        final query = db.select(db.trainingTable)
-          ..orderBy([(tbl) => OrderingTerm.desc(tbl.createdAt)]);
+    return execute(() async {
+      final query = db.select(db.trainingTable)
+        ..orderBy([(tbl) => OrderingTerm.desc(tbl.createdAt)]);
 
-        final res = await query.get();
+      final res = await query.get();
 
-        return Success(res.map(_toDomain).toList());
-      },
-      createDefault: (_) => Failure(DatabaseFailure()),
-    );
+      return Success(res.map(_toDomain).toList());
+    }, createDefault: (_) => Failure(DatabaseFailure()));
   }
 }
